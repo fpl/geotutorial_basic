7564f890aa3f74e4503028780ff5c85f46f5381b
Francesco Paolo Lovergine <frankie@debian.org>
2016-06-10 11:45:45 +0200

    07

---
+++ 0/07_fibonacci.py
@@ -0,0 +1,11 @@
+def fibonacci(n=2000):
+    a, b = 0, 1
+    f = []
+    while b<n:
+        f.append(a)
+        a, b = b, a+b
+    return f
+
+if __name__ == "__main__":
+    s = fibonacci()
+    print s
