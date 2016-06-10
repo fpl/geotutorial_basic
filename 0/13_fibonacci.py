bb3399ea89dfc8897408aad3d2af8d0f9cb1e93a
Francesco Paolo Lovergine <frankie@debian.org>
2016-06-10 12:37:23 +0200

    all the rest

---
+++ 0/13_fibonacci.py
@@ -0,0 +1,22 @@
+'''
+Now use a class instead of a procedural module
+'''
+import sys
+from Fibonacci import Fibonacci as f
+
+def usage():
+    print '''\
+usage: %s int \
+''' % sys.argv[0]
+    sys.exit(1)
+
+if len(sys.argv) < 2:
+    usage()
+
+try:
+    n = int(sys.argv[1])
+except:
+    usage()
+
+s = f(n)
+s.list()
