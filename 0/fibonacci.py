a8a21035f3e570751310967a43536eccc1824ec6
Francesco Paolo Lovergine <frankie@debian.org>
2016-06-10 11:52:08 +0200

    08

--- 0/fibonacci.py
+++ 0/fibonacci.py
@@ -1,15 +1,11 @@
-class Fibonacci:
-    _f = []
-
-    def __init__(self, n=2000):
-        a, b = 0, 1
-        while b<n:
-            self._f.append(a)
-            a, b = b, a+b
-
-    def list(self):
-        print self._f
+def fibonacci(n=2000):
+    a, b = 0, 1
+    f = []
+    while b<n:
+        f.append(a)
+        a, b = b, a+b
+    return f
 
 if __name__ == "__main__":
-    s = Fibonacci(10000)
-    s.list()
+    s = fibonacci()
+    print s
