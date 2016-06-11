7837faa47de9b402b647194fdbcf0ca91e6f3a7f
Francesco Paolo Lovergine <frankie@debian.org>
2016-06-11 16:37:29 +0200

    Fixed JSON format

--- 4/17_json.py
+++ 4/17_json.py
@@ -1,8 +1,19 @@
 # Parse GeoJson data
-jsdata = """{ "type": "Feature", "id": "OpenLayers.Feature.Vector_314", "pro
-perties": {}, "geometry": { "type": "Point", "coordinates": [ 97.03125, 39.72656
-25 ] }, "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.
-3:CRS84" } } }"""
+jsdata = """{ 
+"type": "Feature", 
+"id": "OpenLayers.Feature.Vector_314", 
+"properties": {}, 
+"geometry": { 
+    "type": "Point", 
+    "coordinates": [ 97.03125, 39.7265625 ] 
+}, 
+"crs": { 
+    "type": "name", 
+    "properties": { 
+        "name": "urn:ogc:def:crs:OGC:1.3:CRS84" 
+    } 
+} 
+}"""
 # Try to eval() the data
 point = eval(jsdata)
 print point["geometry"]
