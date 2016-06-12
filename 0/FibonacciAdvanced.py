f0702d87e082699992952d75ab06cc32c1c3d532
Francesco Paolo Lovergine <frankie@debian.org>
2016-06-12 13:23:42 +0200

    Advanced OO approaches for the faint of heart.

--- 0/FibonacciAdvanced.py
+++ 0/FibonacciAdvanced.py
@@ -2,6 +2,10 @@
     FibonacciAdvanced, the OOP approach
 '''
 class FibonacciAdvanced:
+    ''' 
+    This is a class with a static member and 
+    a static method
+    '''
 
     # static member
     _name = 'Fibonacci series advanced class' 
@@ -22,14 +26,67 @@ class FibonacciAdvanced:
     def slice(self,start=0,end=-1):
         print self._f[start:end]
 
+    # static method with mandatory decorator
     @staticmethod 
     def name():
         return FibonacciAdvanced._name
 
+'''
+    This is a base class with inherited classes
+'''
+
+class Sequence(object):
+
+    def __init__(self, a, b, n=2000):
+        self._f = []
+        while b<n:
+            self._f.append(a)
+            a, b = b, a+b
+
+    def list(self):
+        print self._f
+
+    def get(self,index=0):
+        print self._f[index]
+
+    def slice(self,start=0,end=-1):
+        print self._f[start:end]
+
+class Fibonacci(Sequence):
+
+    def __init__(self, n=2000):
+        Sequence.__init__(self,0,1,n)
+
+class Lucas(Sequence):
+
+    def __init__(self, n=2000):
+        Sequence.__init__(self,2,1,n)
+
+class FibonacciSuper(Sequence):
+    def __init__(self, n=2000):
+        super(FibonacciSuper,self).__init__(2,1,n)
+
+#
+# ... and all together
+#
+
 if __name__ == "__main__":
-    s = FibonacciAdvanced(10000)
+    u = FibonacciAdvanced(10000)
+    s = Fibonacci(10000)
+    t = Lucas(100000)
+    v = FibonacciSuper(1000000000000000000)
+
+    u.list()
+    print u.name()
+
     s.list()
     s.get()
     s.get(11)
     s.slice(11,15)
-    print s.name()
+
+    t.list()
+    t.get()
+    t.get(11)
+    t.slice(11,15)
+
+    v.list()
