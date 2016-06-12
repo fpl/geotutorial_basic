aca6ade5023e02813a772ae86d90e6cb0b0122b1
Francesco Paolo Lovergine <frankie@debian.org>
2016-06-12 16:51:46 +0200

    Adding world borders data

---
+++ files/world_borders.php
@@ -0,0 +1,291 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en">
+
+<head>
+  	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+  	<meta http-equiv="cache-control" content="no-cache" />
+  	<meta http-equiv="expires" content="3600" />
+  	<meta name="revisit-after" content="2 days" />
+  	<meta name="robots" content="index,follow" />
+  	<meta name="publisher" content="Your publisher infos here ..." />
+  	<meta name="copyright" content="Your copyright infos here ..." />
+  	<meta name="author" content="Author: Your author infos here ..." />
+  	<meta name="distribution" content="global" />
+  	<meta name="description" content="Your page description here ..." />
+  	<meta name="keywords" content="Your keywords, keywords, keywords, here ..." />
+
+	
+
+  	<link rel="stylesheet" type="text/css" media="screen,projection,print" href="http://thematicmapping.org/css/global.css" />
+
+  	<link rel="stylesheet" type="text/css" media="screen,projection,print" href="http://thematicmapping.org/css/layout4_setup.css" />
+  	<link rel="stylesheet" type="text/css" media="screen,projection,print" href="http://thematicmapping.org/css/layout4_text.css" />
+  	<link rel="stylesheet" type="text/css" media="screen,projection,print" href="http://thematicmapping.org/css/thematicmapping_old.css" />
+
+
+  	<link rel="icon" type="image/x-icon" href="./img/favicon.ico" />
+  	<title>thematicmapping.org</title>
+</head>
+
+<!-- Global IE fix to avoid layout crash when single word size wider than column width -->
+<!--[if IE]><style type="text/css"> body {word-wrap: break-word;}</style><![endif]-->
+
+<body>
+	<div class="page-container">
+		<div class="header">
+
+      		<div class="header-top">
+        		<div class="sitename">
+          			<h1><a href="http://thematicmapping.org" title="Go to start page">thematic<span style="font-weight:normal;">mapping</span><sup>.org</sup></a></h1>
+        		</div>
+      		</div>
+      
+      		<div class="header-middle">    
+        		<div class="sitemessage">
+          			<h1>Thematic Mapping API</h1>
+          			<h2>Create KML based thematic maps<br/>with a few lines of JavaScript</h2>
+          			<h3><a href="http://thematicmapping.org/api">&rsaquo;&rsaquo;&nbsp;More details</a></h3>
+        		</div>        
+      		</div>
+      
+     	 	<div class="header-bottom">
+      
+        	<!-- Navigation Level 2 (Drop-down menus) -->
+        	<div class="nav2">
+          		<ul>
+            		<li><a href="http://thematicmapping.org">Home</a></li>
+          		</ul>
+	          	<ul>
+		            <li><a href="http://thematicmapping.org/api">API<!--[if IE 7]><!--></a><!--<![endif]-->
+		              	<!--[if lte IE 6]><table><tr><td><![endif]-->
+		                <ul>
+		                  <li><a href="http://thematicmapping.org/api">Introduction</a></li>
+		                  <li><a href="http://thematicmapping.org/api/docs.php">Documentation</a></li>
+		                  <li><a href="http://thematicmapping.org/api/examples.php">Examples</a></li>
+		                  <li><a href="http://groups.google.com/group/thematic-mapping-api">Forum</a></li>
+		                </ul>
+		              	<!--[if lte IE 6]></td></tr></table></a><![endif]-->
+		            </li>
+		        </ul>   
+		        <ul>
+		            <li><a href="http://thematicmapping.org/engine">Engine</a></li>
+		        </ul>
+		        <ul>
+		            <li><a href="http://thematicmapping.org/techniques">Techniques</a></li>
+		        </ul>
+		        <ul>
+		            <li><a href="http://thematicmapping.org/playground">Playground</a></li>
+		        </ul>
+		        <ul>
+		            <li><a href="http://thematicmapping.org/downloads">Downloads</a></li>
+		        </ul>
+		        <ul>
+		            <li><a href="http://blog.thematicmapping.org">Blog</a></li>
+		        </ul>
+		        <ul>
+		            <li><a href="http://thematicmapping.org/about">About</a></li>
+		        </ul>
+		    </div>
+		</div>
+
+      	<!-- A.4 HEADER BREADCRUMBS -->
+
+      	<!-- Breadcrumbs -->
+      	<div class="header-breadcrumbs">
+        	<ul>
+          		<li><a href="http://thematicmapping.org">Home</a></li>
+		  		<li><a href="http://thematicmapping.org/downloads">Downloads</a></li> <li>World Borders Dataset</li>         	</ul>
+
+        	<!-- Search form -->                  
+        	<div class="searchform">
+
+				<form action="http://www.google.com/cse" id="cse-search-box" class="form">
+			  		<fieldset>
+			    		<input type="hidden" name="cx" value="008370996709983572748:_odelzthrpq" />
+			    		<input type="hidden" name="ie" value="UTF-8" />
+			    		<input type="text" name="q" class="field" />
+			    		<input type="submit" name="sa" value="GO!" class="button" />
+			  		</fieldset>
+				</form>
+
+				<script type="text/javascript" src="http://www.google.com/coop/cse/brand?form=cse-search-box&lang=en"></script>
+
+        	</div>
+		
+      	</div>
+    </div>
+
+<div class="main">
+
+    <!-- B.1 MAIN NAVIGATION -->
+    <div class="main-navigation">
+
+        <!-- Navigation Level 3 -->
+        <div class="round-border-topright"></div>
+        <h1 class="first">Downloads</h1>
+
+        <!-- Navigation with grid style -->
+        <dl class="nav3-grid">
+          <dt><a href="http://code.google.com/p/thematicmapping/">Thematic Mapping Engine</a></dt>
+          <dt><a href="/downloads/world_borders.php">World Borders Dataset</a></dt>
+          <dt><a href="http://code.google.com/p/ext-js-google-earth-api/">Ext.ux.GEarthPanel</a></dt>
+        </dl>                        
+
+    </div>
+ 
+    <div class="main-content">
+        
+        <h1 class="pagetitle">World Borders Dataset</h1>
+
+	    <p>Provided by Bjorn Sandvik, <a href="/">thematicmapping.org</a></p>
+	
+	    <p>If you have any comments about this dataset, please post them on <a href="http://blog.thematicmapping.org/2008/03/world-borders-for-thematic-web-mapping.html">this page</a>.</p>
+	
+	    <p>Use this dataset with care, as several of the borders are disputed.</p>
+	
+	    <p>The original shapefile (world_borders.zip, 3.2 MB) was downloaded from the Mapping Hacks website:
+	    <a href="http://www.mappinghacks.com/data/">http://www.mappinghacks.com/data/</a></p>
+	
+	    <p>The dataset is available under a Creative Commons <a href="http://creativecommons.org/licenses/by-sa/3.0/">Attribution-Share Alike License</a>. If you use this dataset, please provide a link to this website.</p>
+	
+	    <h2>Main changes</h2>
+	    <ul>
+	        <li> Polygons representing one country/area are merged into one feature.</li>
+	        <li> Added ISO 3166-1 Country codes (alpha-2, alpha-3, numeric-3).</li>
+	        <li> Various feature changes to make the dataset more compatible with ISO 3611-1. See "Readme.txt" for details.</li>
+	        <li> Added <a href="http://unstats.un.org/unsd/methods/m49/m49regin.htm">region and sub-region codes</a> from UN Statistical Division.</li>
+	        <li> Added longitude/latitude values for each country.</li>
+	    </ul>
+	
+	    <h2>Attribute columns</h2>
+	    <table>
+	        <tr>
+	            <td>COLUMN</td>
+	            <td>TYPE</td>
+	            <td>DESCRIPTION</td>
+	        </tr>
+	        <tr>
+	            <td>Shape</td>
+	            <td>Polygon</td>
+	            <td>Country/area border as polygon(s)</td>
+	        </tr>
+	        <tr>
+	            <td>FIPS</td>
+	            <td>String(2)</td>
+	            <td>FIPS 10-4 Country Code</td>
+	        </tr>
+	        <tr>
+	            <td>ISO2</td>
+	            <td>String(2)</td>
+	            <td>ISO 3166-1 Alpha-2 Country Code</td>
+	        </tr>
+	        <tr>
+	            <td>ISO3</td>
+	            <td>String(3)</td>
+	            <td>ISO 3166-1 Alpha-3 Country Code</td>
+	        </tr>
+	        <tr>
+	            <td>UN</td>
+	            <td nowrap="nowrap">Short Integer(3)</td>
+	            <td>ISO 3166-1 Numeric-3 Country Code</td>
+	        </tr>
+	        <tr>
+	            <td>NAME</td>
+	            <td>String(50)</td>
+	            <td>Name of country/area</td>
+	        </tr>
+	        <tr>
+	            <td>AREA</td>
+	            <td>Long Integer(7)</td>
+	            <td>Land area, FAO Statistics (2002) </td>
+	        </tr>
+	        <tr valign="top">
+	            <td>POP2005</td>
+	            <td>Double(10,0)</td>
+	            <td>Population, World Population Prospects (2005)</td>
+	        </tr>
+	        <tr valign="top">
+	            <td>REGION</td>
+	            <td>Short Integer(3)</td>
+	            <td>Macro geographical (continental region), UN Statistics</td>
+	        </tr>
+	        <tr valign="top">
+	            <td>SUBREGION</td>
+	            <td>Short Integer(3)</td>
+	            <td>Geographical sub-region, UN Statistics</td>
+	        </tr>
+	        <tr>
+	            <td>LON</td>
+	            <td>FLOAT (7,3)</td>
+	            <td>Longitude</td>
+	        </tr>
+	        <tr>
+	            <td>LAT</td>
+	            <td>FLOAT (6,3)</td>
+	            <td>Latitude</td>
+	        </tr>
+	    </table>
+	
+	    <h2>Download</h2>
+	    <p>The dataset is available in shapefile format:</p>
+	    <ul>
+	        <li> <a href="TM_WORLD_BORDERS-0.3.zip">TM_WORLD_BORDERS-0.3.zip</a> (3,380 kB)</li>
+	        <li> <a href="TM_WORLD_BORDERS_SIMPL-0.3.zip">TM_WORLD_BORDERS_SIMPL-0.3.zip</a> (226 kB)</li>
+	    </ul>
+
+
+<div id="content_right">
+    <h2 style="margin-top:0px">How can this dataset be used?</h2>
+    <a href="/engine"><img src="images/tmafrica.png" width="240" height="180" /></a>
+    <p>The dataset is part of the <a href="/engine">Thematic Mapping Engine</a> and you can see several examples on my <a href="http://blog.thematicmapping.org">thematic mapping blog</a>.</p>
+    <br/>
+    <a href="http://www.icgg.org/corruption.cpi_2008_worldmap.html"><img src="images/corruption240px.png" width="240" height="154" /></a>
+    <p><a href="http://www.icgg.org/">Internet Center for Corruption Research</a> has made <a href="http://www.icgg.org/corruption.cpi_2008_worldmap.html">an interactive map</a> showing the Corruption Perceptions Index.</p>
+    <br/>
+    <a href="http://earth.google.com/outreach/kml_entry.html#tDisappearing%20Forests"><img src="images/deforestation.png" width="240" height="180" /></a>
+    <p><a href="http://david.tryse.net/googleearth/">David Tryse</a> has made a <a href="http://earth.google.com/outreach/kml_entry.html#tDisappearing%20Forests">disappearing forests visualisation</a> in Google Earth.</p>
+    <br/>
+    <a href="http://gvlt.wordpress.com/2008/05/17/tutorial-thematic-mapping-with-the-google-maps-flash-api/"><img src="images/googleflashmapsapi.png" width="240" height="180" /></a>
+    <p><a href="http://gvlt.wordpress.com/">Guilhem</a> shows <a href="http://gvlt.wordpress.com/2008/05/17/tutorial-thematic-mapping-with-the-google-maps-flash-api/">how choropleth maps can be created with Google Maps Flash API</a>.</p>
+</div>
+
+	</div>
+
+    <!-- B.3 SUBCONTENT -->
+    <div class="main-subcontent">
+
+        <!-- Subcontent unit -->
+        <div class="subcontent-unit-border-orange">
+        	<div class="round-border-topleft"></div><div class="round-border-topright"></div>
+          	<h1 class="orange">Support this site</h1>
+ 			<p>If this site has benefited you in some way, please consider donating.</p>
+			<form action="https://www.paypal.com/cgi-bin/webscr" method="post" style="text-align: center;">
+				<input value="_s-xclick" name="cmd" type="hidden"/>
+				<input value="1750357" name="hosted_button_id" type="hidden"/>
+				<input border="0" alt="" src="https://www.paypal.com/en_GB/i/btn/btn_donate_LG.gif" name="submit" type="image"/>
+				<!--<img border="0" alt="" width="1" src="https://www.paypal.com/en_US/i/scr/pixel.gif" height="1" style="border: none;"/>-->
+			</form>        
+		</div>
+
+	</div>	
+	
+</div>
+
+    <div class="footer">
+      <p>Copyright &copy; 2009 Bjørn Sandvik | All Rights Reserved</p>
+      <p class="credits">Based on design template by <a href="http://www.1-2-3-4.info" title="Designer Homepage">G. Wolfgang</a> | <a href="http://validator.w3.org/check?uri=referer" title="Validate XHTML code">W3C XHTML 1.0</a> | <a href="http://jigsaw.w3.org/css-validator/" title="Validate CSS code">W3C CSS 2.0</a> </p>
+    </div>      
+  </div> 
+ 
+<script type='text/javascript'>
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type='text/javascript'>
+var pageTracker = _gat._getTracker("UA-1478729-4");
+pageTracker._initData();
+pageTracker._trackPageview();
+</script>
+  
+</body>
+</html>
